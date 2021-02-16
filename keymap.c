9b7121049fc999317375cc2cabe48255ef046bc5
rkqvavleuwvuqdav <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2021-02-15 16:44:08 -0800

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -41,10 +41,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT_all(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
 		KC_TAB,  KC_Q,    ALT_W,    CMD_E,    KC_R,    KC_T,    KC_Y,    KC_U,    CMD_I,    ALT_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 		CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUOT, KC_NUHS, KC_ENT,
-		KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT_RSFT_ENT, MO(1),
+		KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT_RSFT_ENT, MO(4),
 		                           KC_LALT, LCMD_RIGHT, LOWER,  KC_SPC,  SPC_NUM,  RAISE,  RCMD_LEFT, RALT_DOWN
     ),
 
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT_all(
-		RESET,   _______, _______, _______, _______, _______, KC_WWW_BACK, KC_WWW_FORWARD, _______, _______, _______, _______, _______, _______, _______,
+		RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
 		_______, _______, _______, _______, _______, _______, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -70,7 +70,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, KC_1, KC_2,    KC_3,    KC_4,    KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______, _______, _______,
 		_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DOT, KC_F12, _______,
 		                           _______, _______, _______, _______, _______, _______, _______, _______
-    )
+    ),
+
+
+		[4] = LAYOUT_all(
+		_______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, KC_WBAK, KC_WSTP, KC_WREF, KC_WFWD, _______, _______, _______, _______,
+		_______, RGB_MOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______,
+				                           _______, _______, _______, _______, _______, _______, _______, _______
+		  )
+
 
 
 
